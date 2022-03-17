7a55df29907e33542a04fb7a6a803b5b08024299
Winnie Mandela <95540890+TechMadi@users.noreply.github.com>
2022-03-18 00:49:21 +0300

    editing Main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int nld);
-void jack_bauer(void);
-void times_table(void);
-int add(int one, int two);
-void print_to_98(int n);
-void print_times_table(int n);
+#include <stdio.h>
 
-#endif /*MAIN_H */
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* HOLBERTON_H */
